8b1d20ff42fe47ab90de2f9da7cb4c59e8ff5060
AntonioMinkowski <antonio1384minkowski@zohomail.eu>
2022-03-24 17:11:55 +0430

    Show unique values from each column that have less than 20 unique values

--- modules/unique_values.py
+++ modules/unique_values.py
@@ -1,10 +1,10 @@
-# Show unique values fromeach columns which have less than 20 unique values
+# Show unique values from each column that have less than 20 unique values
 def unique_values(data):
     for col in data.columns:
         temp = data[col]
         n_unique = temp.nunique()
 
         if n_unique < 20:
-            print(f'==========={col}===========\n')
+            print(f'=========== Unique values of {col} column ===========\n')
             for unique in temp.unique():
                 print(unique)
